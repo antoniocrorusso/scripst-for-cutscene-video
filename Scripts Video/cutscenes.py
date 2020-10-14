c2941df0ed223f1c88c6bceb3488ba7ae69a78fc
antoniocrorusso <56083560+antoniocrorusso@users.noreply.github.com>
2020-10-14 18:13:27 -0300

    Scripts

---
+++ Scripts Video/cutscenes.py
@@ -0,0 +1,65 @@
+from gerenciador import *
+from dicionarios import *
+
+class Cutscene:
+    def __init__(self, falas, fala_final):
+
+        # Variáveis da Cutscene
+        self.timer = pygame.time.get_ticks()
+
+        self.fala_final = fala_final
+        self.etapa = 0
+        self.falas = falas
+
+        # Parametros para escrever a fala na tela
+        self.velocidade_fala = 0.2
+        self.inicio_fala = 0
+        self.numero_fala = self.falas[0]
+
+        # Parametros da funcao colocar_texto
+        self.tamanho = 40
+        self.cor = BRANCO
+        self.pos_x = 50
+        self.pos_y = 50
+
+        self.cutscene_running = True
+
+
+    def update(self):
+        apertar = pygame.key.get_pressed()
+        espaco = apertar[pygame.K_SPACE]
+
+        # print(self.numero_fala)
+        if self.etapa < self.fala_final:
+            if int(self.inicio_fala) < len(self.numero_fala):
+                self.inicio_fala += self.velocidade_fala
+            else:
+                if espaco:
+                    self.etapa += 1
+                    self.inicio_fala = 0
+                    self.numero_fala = self.falas[self.etapa]
+
+
+        elif self.etapa >= self.fala_final:
+            if int(self.inicio_fala) < len(self.numero_fala):
+                self.inicio_fala += self.velocidade_fala
+            else:
+                if espaco:
+                    self.cutscene_running = False
+
+        return self.cutscene_running
+
+    def draw(self, janela):
+
+        text = self.numero_fala
+        colocar_texto(
+            janela,
+            text[0:int(self.inicio_fala)],
+            self.tamanho,
+            self.cor,
+            self.pos_x,
+            self.pos_y
+        )
+
+
+
---
+++ Scripts Video/gerenciador.py
@@ -0,0 +1,43 @@
+import pygame
+
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+
+def colocar_texto(tela, texto, tamanho, cor, pos_x, pos_y):
+    font = pygame.font.SysFont(None, tamanho)
+    text_surface = font.render(texto, True, cor)
+    text_rect = text_surface.get_rect()
+    text_rect.topleft = (pos_x, pos_y)
+    tela.blit(text_surface, text_rect)
+
+class Gerenciador:
+    def __init__(self, janela):
+        self.cut_scene = None
+        self.cut_scene_runnig = False
+
+        # Variáveis
+        self.janela = janela
+        self.tamanho_janela = 0
+
+    def cut_scene_start(self, cut_scene):
+        self.cut_scene = cut_scene
+        self.cut_scene_running = True
+
+    def cut_scene_end(self):
+        self.cut_scene = None
+        self.cut_scene_running = False
+
+    def update(self):
+
+        if self.cut_scene_running:
+            if self.tamanho_janela < self.janela.get_height() * 0.3:
+                self.tamanho_janela += 2
+            self.cut_scene_running = self.cut_scene.update()
+        else:
+            self.cut_scene_end()
+
+    def draw(self):
+        if self.cut_scene_running:
+            pygame.draw.rect(self.janela, PRETO, (0, 0, self.janela.get_width(), self.tamanho_janela))
+
+            self.cut_scene.draw(self.janela)
